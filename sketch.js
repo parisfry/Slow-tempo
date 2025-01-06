9a2f9d0351e77c0fa2c838de5c0229aec56a5bf2
parisfry <p.fry0220221@arts.ac.uk>
2025-01-06 14:13:15 +0000

    Medium tempo
    Changing the values of my code to represent a slightly faster tempo

--- sketch.js
+++ sketch.js
@@ -1,32 +1,30 @@
-let steps = 360 * 100;//increasing the number of steps to ensure a smoother shape 
-let r = 100; //decreasing the radius to have a smaller shape
-let noiseScale = 0.003; //decreasing the noiseScale to create smooter movements
-const noiseAmount = 300; //increasing the noise amount to have more movement within the shape 
+let steps = 360 * 1; //decreasing the number of steps to create more jagged movements 
+let r = 32; //decreaing the radius of the shape
+let noiseScale = 0.02; //increasing the noise scale to create more movement
+const noiseAmount = 350; //increasing the noise amount to create more movement
  
 function setup() {
  createCanvas(400, 400);
  noFill();
- stroke(255, 50); //stroke colour white whith low opacity to create an echo effect
- background(200);//changing the background value to a lighter shade of greyscale 
+ stroke(255, 50);
+ background(0);
+ r = 400 / 2.5;
 }
- 
-function distortedCircle() {
- beginShape();
- for (let i = 0; i <= steps; i++) {
-   x = width / 2 + r * cos((TWO_PI * i) / steps);
-   y = height / 2 + r * sin((TWO_PI * i) / steps);
-   x += map(
-     noise(noiseScale * x, noiseScale * y, frameCount / 100),0,1,-noiseAmount,noiseAmount);//changing the framecount to make the movements slower
-   y += map(
-     noise(noiseScale * x, noiseScale * y, 1), 0,1, -noiseAmount,noiseAmount);
-   vertex(x, y);
- }
- endShape();
+
+function distortedWave() { //changing the shape to distortedWave to have more interesting free flowing lines
+  beginShape();
+  for (let i = 0; i <= steps; i++) {
+    x = width / 2 + r * cos((TWO_PI * i) / steps);
+    y = height / 2 + r * sin((TWO_PI * i) / steps); 
+    x += map(noise(noiseScale * x, noiseScale * y, frameCount / 700), 0, 1, -noiseAmount, noiseAmount);
+    y += map(noise(noiseScale * x, noiseScale * y, 1), 0, 1, -noiseAmount, noiseAmount);
+    vertex(x, y);
+  }
+  endShape();
 }
  
 function draw() {
- background(0,1); //adding opacity to create an echo effect
- stroke(255); //draws the distorted circle in white
- distortedCircle();
+ background(200,20);//changing the background colour to create more contrast 
+ stroke(25);
+ distortedWave();
 }
- 
